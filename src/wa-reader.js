e03148ed0bed15d1669a1809b5d019c0dd7a4aa7
Kusmayadi <kusmayadi@gmail.com>
2017-01-07 23:23:32 +0700

    Initial working version

---
+++ src/wa-reader.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var fs = require('fs'),
+  grok = require('node-grok');
+
+const ERRORS = {
+  'invalidFile': { Error: 'File is not a valid WhatsApp chat file.', errno: -1, code: 'INVALID_FILE'}
+};
+
+module.exports = {
+  parse: function(chatText, lastDate, callback) {
+    if (typeof(lastDate) == 'function') {
+      callback = lastDate;
+      lastDate = null;
+    }
+
+    var err = null;
+
+    var patterns = grok.loadDefaultSync();
+    patterns.createPattern('%{HOUR}:%{MINUTE}', 'WA_TIME');
+    patterns.createPattern('(.*\n)+', 'WA_CHAT')
+
+    var waChat  = '%{DATE:date}, %{WA_TIME:time} - %{DATA:sender}: %{WA_CHAT:chat}';
+    var waSystem = '%{DATE:date}, %{WA_TIME:time} - %{WA_CHAT:chat}';
+
+    var waChatPattern = patterns.createPattern(waChat);
+    var waSystemPattern = patterns.createPattern(waSystem);
+
+    if (lastDate)
+    {
+      lastDate = lastDate.replace(' ', ', ');
+      chatText = chatText.substr(chatText.indexOf(lastDate));
+    }
+
+    var conversations = chatText.split(/(?=[0-9]+\/[0-9]+\/[0-9]+, )/);
+    var chats = [];
+    var tempChat = '';
+    var mergeChat = '';
+
+    if (conversations.length <= 1) {
+      callback(ERRORS.invalidFile);
+    } else {
+      conversations.forEach(function(chat, index) {
+        mergeChat = tempChat + chat;
+
+        if (chat.length == '1') {
+          tempChat = '1';
+        }
+        else
+        {
+          var chatObj = waChatPattern.parseSync(mergeChat);
+
+          if (chatObj == null)
+            chatObj = waSystemPattern.parseSync(mergeChat);
+
+          chats.push(chatObj);
+          tempChat = '';
+        }
+      });
+
+      callback(err, chats);
+    }
+  },
+
+  read: function(chatFile, lastDate, callback) {
+    if (typeof(lastDate) == 'function') {
+      callback = lastDate;
+      lastDate = null;
+    }
+
+    var err = null;
+
+    fs.readFile(chatFile, 'utf-8', (err, text) => {
+      if (err) {
+        callback(err);
+      } else {
+        module.exports.parse(text, lastDate, (err, conversations) => {
+          callback(err, conversations);
+        });
+      }
+    });
+  }
+}
---
+++ tests/wa-reader.js
@@ -0,0 +1,103 @@
+var should = require('should'),
+  wa = require('../src/wa-reader'),
+  path = require('path'),
+  fs = require('fs');
+
+describe('wa-reader', function() {
+  var chatFile = path.join(path.dirname(__filename), '/chats.txt');
+  var notChatFile = path.join(path.dirname(__filename), '/not-wa-chats.txt');
+  var oneLineChatFile = path.join(path.dirname(__filename), '/one-line-chats.txt');
+  var chatFileNotExists = path.join(path.dirname(__filename), '/not-exists.txt');
+
+  describe('parse', function() {
+    var chatText = fs.readFileSync(chatFile, 'utf-8');
+    var notChatText = fs.readFileSync(notChatFile, 'utf-8');
+    var oneLineChatText = fs.readFileSync(oneLineChatFile, 'utf-8');
+
+    it('should return an array', function(done) {
+      wa.parse(chatText, function(err, conversations) {
+        conversations.should.be.instanceOf(Array);
+        done();
+      });
+    });
+
+    it('should return an array of last chats based on specific date & time', function(done) {
+      wa.parse(chatText, '25/12/16 20:39', function(err, conversations) {
+        conversations.length.should.be.equal(4);
+        done();
+      });
+    });
+
+    it('should return an array of object', function(done) {
+      wa.parse(chatText, function(err, conversations) {
+        conversations.should.be.instanceOf(Object);
+        done();
+      });
+    });
+
+    it('should return an array of object with date key', function(done) {
+      wa.parse(chatText, function(err, conversations) {
+        conversations[0].should.have.property('date');
+        done();
+      });
+    });
+
+    it('should return an array of object with time key', function(done) {
+      wa.parse(chatText, function(err, conversations) {
+        conversations[0].should.have.property('time');
+        done();
+      });
+    });
+
+    it('should return an array of object with sender key', function(done) {
+      wa.parse(chatText, function(err, conversations) {
+        conversations[1].should.have.property('sender');
+        done();
+      });
+    });
+
+    it('should return an array of object with chat key', function(done) {
+      wa.parse(chatText, function(err, conversations) {
+        conversations[1].should.have.property('chat');
+        done();
+      });
+    });
+
+    it('should return error if text is not WhatsApp chat format', function(done) {
+      wa.parse(notChatText, function(err, conversations) {
+        err.should.have.property('code', 'INVALID_FILE');
+        done();
+      });
+    });
+
+    it('should return an array of object with chat key even if chats is only has one line chat', function(done) {
+      wa.parse(oneLineChatText, function(err, conversations) {
+        conversations[0].should.have.property('chat');
+        done();
+      });
+    });
+  });
+
+  describe('read', function() {
+    it('should return an array', function(done) {
+      wa.read(chatFile, function(err, conversations) {
+        conversations.should.be.instanceOf(Array);
+        done();
+      });
+    });
+
+    it('should return error if file is not exists', function(done) {
+      wa.read(chatFileNotExists, function(err, conversations) {
+        err.should.have.property('code', 'ENOENT');
+        done();
+      });
+    });
+
+    it('should return error if file is not WhatsApp chat file', function(done) {
+      wa.read(notChatFile, function(err, conversations) {
+        err.should.have.property('code', 'INVALID_FILE');
+        done();
+      });
+    });
+  });
+});
